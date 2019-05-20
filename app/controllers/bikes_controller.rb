ece7b9bbf5bac78864fbaff8ee8dd8ecc8e60a24
NicolasDontigny <nicolas.dontigny1@gmail.com>
2019-05-20 16:24:50 -0400

    random

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -11,7 +11,6 @@ class BikesController < ApplicationController
   def index_owner
     @current_user = current_user
     @bikes = Bike.all.where(user_id: @current_user.id)
-    raise
   end
 
   def new
