39a121e20b530c2c3fb203a165a645bec837ac54
Michael Zhu <michaelzhu63@hotmail.com>
2019-05-23 16:04:03 -0400

    Updated avatar upload logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
+  before_create :set_default_avatar
 
   has_many :bookings
   has_many :bikes
@@ -10,12 +11,9 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :postal_code, presence: true
   validates :email, uniqueness: true
-
-  # def check_avatar(user)
-  #   if user.image.nil?
-  #     # return default image
-  #  else
-  #     #return user avatar
-  #  end
-  # end
+  def set_default_avatar
+    # your code
+    user.avatar = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    raise
+  end
 end
