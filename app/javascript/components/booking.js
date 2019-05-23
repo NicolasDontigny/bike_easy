7399144f24e45e225085eedfe21dd992ce7555ab
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-23 06:43:50 -0400

    saving WIP

--- app/javascript/components/booking.js
+++ app/javascript/components/booking.js
@@ -1,37 +1,33 @@
 import flatpickr from 'flatpickr';
 
-// const toggleDateInputs = function() {
-//   const startDateInput = document.getElementById('booking_start_date');
-//   const endDateInput = document.getElementById('booking_end_date');
+const toggleDateInputs = function() {
+  const startDateInput = document.getElementById('booking_start_date');
+  const endDateInput = document.getElementById('booking_end_date');
 
-//   if (startDateInput && endDateInput) {
-//     const unvailableDates = JSON.parse(document.querySelector('.form__wrapper').dataset.unavailable);
+  if (startDateInput && endDateInput) {
+    const unvailableDates = JSON.parse(document.querySelector('.form__wrapper').dataset.unavailable)
 
-//     flatpickr(startDateInput, {
-//     minDate: 'today',
-//     dateFormat: 'Y-m-d',
-//     disable: unvailableDates,
-//     onChange: function(selectedDates, selectedDate) {
-//       if (selectedDate === '') {
-//         endDateInput.disabled = true;
-//       }
-//       let minDate = selectedDates[0];
-//       minDate.setDate(minDate.getDate() + 1);
-//       endDateCalendar.set('minDate', minDate);
-//       endDateInput.disabled = false;
-//     }
-//   });
-//     // const endDateCalendar =
-//     //   flatpickr(endDateInput, {
-//     //     dateFormat: 'd-m-Y',
-//     //     disable: unvailableDates,
-//     //     },
-//     // });
-//   }
-// };
+    flatpickr(startDateInput, {
+    minDate: 'today',
+    dateFormat: 'd-m-Y',
+    disable: unvailableDates,
+    onChange: function(selectedDates, selectedDate) {
+      if (selectedDate === '') {
+        endDateInput.disabled = true;
+      }
+      let minDate = selectedDates[0];
+      minDate.setDate(minDate.getDate() + 1);
+      endDateCalendar.set('minDate', minDate);
+      endDateInput.disabled = false;
+    }
+  });
 
-const toggleDateInputs = () => {
-  console.log('coucou');
-}
+    const endDateCalendar =
+      flatpickr(endDateInput, {
+        dateFormat: 'd-m-Y',
+        disable: unvailableDates,
+    });
+  }
+};
 
 export { toggleDateInputs }
