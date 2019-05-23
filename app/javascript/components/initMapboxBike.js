3c33d5efdfed8aaa52ac9b0bfbd0c6f501dd50f2
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-23 13:48:26 -0400

    saving map

--- app/javascript/components/initMapboxBike.js
+++ app/javascript/components/initMapboxBike.js
@@ -1,34 +1,28 @@
 import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers => bounds.extend([ marker.lng, marker.lat ]);
-  map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 0 });
-};
-
 const initMapboxBike = () => {
 
   const mapElement = document.getElementById('map_bike');
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const markers = JSON.parse(mapElement.dataset.markers);
     const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      container: 'map_bike',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      minZoom: 14,
+      center: [markers.lng, markers.lat]
     });
 
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken }));
 
-    const markers = JSON.parse(mapElement.dataset.markers);
     const popup = new mapboxgl.Popup().setHTML(markers.infoWindow);
 
     new mapboxgl.Marker()
       .setLngLat([ markers.lng, markers.lat ])
       .setPopup(popup)
       .addTo(map);
-
-    fitMapToMarkers(map, markers);
   }
 };
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,3 +34,5 @@ initMapbox();
 initAutocomplete();
 
 toggleDateInputs();
+
+initMapboxBike();
