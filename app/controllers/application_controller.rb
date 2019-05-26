512f682359bea02c618e15ecef16fb4ac2c5ab0e
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-26 14:35:19 -0400

    wip to fix http avatar upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :postal_code])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :postal_code, :avatar, :avatar_cache])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :postal_code, :avatar, :avatar_cache, :remove_avatar])
