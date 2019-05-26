c8e0ae69540c7ddb82d542d25bd957744281273a
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2019-05-26 18:04:39 -0400

    fix http avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,13 @@
 module ApplicationHelper
-    def avatar_for(user)
-        @avatar = user.avatar
-        if @avatar.nil?
-            @avatar_user = image_tag("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
-        else
-            if !@avatar.url
-                @avatar_user = image_tag("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
-            else
-                @avatar_user = image_tag(@avatar.url)
-            end
-        end
+  def avatar_for(user)
+    @avatar = user.avatar
+    if @avatar.nil?
+      image_tag("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
+    elsif !@avatar.url
+      image_tag("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
+    else
+      # image_tag(@avatar.url)
+      cl_image_tag(@avatar.url, secure: true)
     end
+  end
 end
